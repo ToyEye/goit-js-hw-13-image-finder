820506ab0a9e2b6baf6346fddf57c0f2df1eec44
Korotenko Alex <alexey.korotenko90@gmail.com>
2021-11-05 16:14:57 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import './sass/main.scss';
+import  apiSrevice  from "./partials/js/apiService.js";
+import  gallery  from "./partials/templates/gallery.hbs";
+
+const form = document.querySelector('#search-form');
+const galleryContainer = document.querySelector('.gallery');
+console.log("~ galleryContainer", galleryContainer)
+
+form.addEventListener('submit', handler);
+
+function handler(e) {
+    e.preventDefault();
+    const input = e.target.query;
+    apiSrevice(input.value).then(data => {
+        markUpCards(gallery(data));
+        return;
+    })
+    
+
+    function markUpCards(markup) {
+        galleryContainer.innerHTML = '';
+        if (markup) {
+            galleryContainer.insertAdjacentHTML('afterbegin', markup);
+            return;
+        }
+    }
+}
---
+++ src/partials/js/apiService.js
@@ -0,0 +1,7 @@
+export default function apiSrevice(seach) {
+    const KEY = '24201171-f795c334c12b489d5c6645c6d';
+    
+    return fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${seach}&page=1&per_page=12&key=${KEY}`)
+        .then(response => response.json())
+        .then(data => data);
+    }
